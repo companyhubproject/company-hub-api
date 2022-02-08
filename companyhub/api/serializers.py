9bcc462e3cfba2da4baf992e29b9a55e293ee4d1
Helena Magaldi <helenamagaldi@gmail.com>
2022-02-08 08:21:21 -0800

    Enviando alterações iniciais

---
+++ companyhub/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Company
+
+
+class CompaniesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = "__all__"
