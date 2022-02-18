627b0166815a8b0d5bb2e7f3a5ef6467020666af
Helena Magaldi <helenamagaldi@gmail.com>
2022-02-18 14:14:09 -0300

    WIP

--- companyhub/api/urls.py
+++ companyhub/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from api import views
 
 
 urlpatterns = [
-    path("companies", views.list_and_create_company)
+    path("companies", views.list_and_create_company),
+    path("companies/<int:pk>/", views.get_and_delete_single_company)
+    # re_path(r"^companies$", views.list_and_create_company),
+    # # re_path(r"^companies/(?P<pk>\d+)/$", views.get_and_delete_single_company)
 ]
--- companyhub/api/views.py
+++ companyhub/api/views.py
@@ -1,3 +1,4 @@
+from email.policy import HTTP
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,17 +8,6 @@ from rest_framework import status
 
 from api.models import Company
 from api.serializers import CompaniesSerializer
-
-
-# @api_view(["GET","POST"])
-# @csrf_exempt
-# def list_and_create_costumer(request):
-#     if request.method == "GET":
-#         return HttpResponse(status=200)
-#     elif request.method == "POST":
-#         return HttpResponse(status=201)
-#     else:
-#         return HttpResponse(status=405)
 
 
 @csrf_exempt
@@ -36,25 +26,39 @@ def list_and_create_company(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse({"companies": serializer.data}, status=status.HTTP_201_CREATED)
+        
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    
+@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
+def get_and_delete_single_company(request, pk):
+        
+    try:
+        company = Company.objects.get(pk)
+    except Company.DoesNotExist:
+        return JsonResponse({"message": "The company does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        company_serializer = CompaniesSerializer(company)
+        return JsonResponse(company_serializer.data)
+
+    elif request.method == "PUT":
+        company_data = JSONParser().parse(request)
+        company_serializer = CompaniesSerializer(company, data=company_data)
+        if company_serializer.is_valid():
+            company_serializer.save()
+            return JsonResponse(company_serializer.data)
+        return JsonResponse(company_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+    # elif request.method == "PUT":
 
+    #     serializer = CompaniesSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse({"companies": serializer.data},status=status.HTTP_201_CREATED)
+    #     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
-        # data = JSONParser().parse(stream=request,media_type="application/json")
-        # serializer = CompaniesSerializer(data=data)
-        # if serializer.is_valid():
-        #     company = serializer.save()
-        #     response = HttpResponse(status=201)
-        #     response["Location"] = "/api/companies/" + str(company.id)
-
-        #     return response
-    
 
 
 
