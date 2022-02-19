92e904a634a96aa93f5bd300be9ea94866623cf2
Helena Magaldi <helenamagaldi@gmail.com>
2022-02-19 12:04:49 -0300

    feat: add get single company and DELETE method

--- companyhub/api/urls.py
+++ companyhub/api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 urlpatterns = [
     path("companies", views.list_and_create_company),
-    path("companies/<int:pk>/", views.get_and_delete_single_company)
+    path("companies/<int:pk>", views.get_and_delete_single_company)
     # re_path(r"^companies$", views.list_and_create_company),
     # # re_path(r"^companies/(?P<pk>\d+)/$", views.get_and_delete_single_company)
 ]
--- companyhub/api/views.py
+++ companyhub/api/views.py
@@ -26,15 +26,15 @@ def list_and_create_company(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse({"companies": serializer.data}, status=status.HTTP_201_CREATED)
-        
+
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(["GET", "PUT", "DELETE"])
 def get_and_delete_single_company(request, pk):
-        
+
     try:
-        company = Company.objects.get(pk)
+        company = Company.objects.get(pk=pk)
     except Company.DoesNotExist:
         return JsonResponse({"message": "The company does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
@@ -50,14 +50,9 @@ def get_and_delete_single_company(request, pk):
             return JsonResponse(company_serializer.data)
         return JsonResponse(company_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    # elif request.method == "PUT":
-
-    #     serializer = CompaniesSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return JsonResponse({"companies": serializer.data},status=status.HTTP_201_CREATED)
-    #     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        company.delete()
+        return JsonResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 
